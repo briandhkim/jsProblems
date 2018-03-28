fa6d905ea77b0483a1f5a3bd2d09aab4bc4828cf
briandhkim <w1057216@gmail.com>
2018-03-27 21:44:49 -0700

    first problem

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+//basic version
+// function reverse(str) {
+// 	const newStr = str.split('').reverse().join('');
+// 	return newStr;
+// }
+
+// function reverse(str){
+// 	let newStr = '';
+// 	for(let char of str){
+// 		newStr = char+newStr;
+// 	}
+// 	return newStr;
+// }
+
+// function reverse(str){
+// 	return str.split('').reduce((reversed, char)=>{
+// 		return char+reversed;
+// 	}, '');
+// }
+
+function reverse(str) {
+	let newStr = '';
+	for(var i=str.length-1; i>=0; i--){
+		newStr+=str[i];
+	}
+	return newStr;
+}
 
 module.exports = reverse;
