659074e62ba49ad458efdbf260504220afc0d22e
briandhkim <w1057216@gmail.com>
2018-03-27 22:51:11 -0700

    maxchar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	let count = {};
+  	for (let i in str){
+    	if( count[str[i]] ){
+      		count[str[i]] += 1;
+    	}else{
+      		count[str[i]] = 1;
+    	}
+  	}
+
+  	let counter = 0;
+  	let returnChar = '';
+  	for (let s in count){
+  		if(count[s]>counter){
+  			counter = count[s];
+  			returnChar = s;
+  		}
+  	}
+
+  	return returnChar;
+}
 
 module.exports = maxChar;
