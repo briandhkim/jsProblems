9664cf90e11d8c056f31ba7af51c1984bf68e26a
briandhkim <w1057216@gmail.com>
2018-04-03 15:21:48 -0700

    vowel problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,16 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+	const inputStr = str.toLowerCase();
+    let counter = 0;
+    let reg = /[aeiou]/;
+    for(let i=0; i<inputStr.length; i++){
+      	if(reg.exec(inputStr[i])){
+        	counter++;
+      	}
+    }
+  	return counter;
+}
 
 module.exports = vowels;
