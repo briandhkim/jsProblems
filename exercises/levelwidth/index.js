1d6baa222a8acf062c8d706d783790ab48058a0b
briandhkim <w1057216@gmail.com>
2018-05-01 17:54:20 -0700

    levelwidth for tree compl

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+	const counter = [0];
+	const arr = [ root, 'mark' ];
+
+	while(arr.length > 1){
+		const elmt = arr.shift();
+		if(elmt==='mark'){
+			counter.push(0);
+			arr.push(elmt);
+		}else{
+			arr.push(...elmt.children);
+			counter[counter.length-1] += 1;
+		}
+	}
+
+	return counter;
+}
 
 module.exports = levelWidth;
