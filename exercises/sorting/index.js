f9b35ed33f46e53238a0af7573704f024c7e8550
briandhkim <w1057216@gmail.com>
2018-05-30 11:46:31 -0700

    buble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,6 +3,17 @@
 
 function bubbleSort(arr) {
 
+	for(let i = 0; i < arr.length; i++){
+		for(let m = 0; m < (arr.length -i-1); m++){
+			if(arr[m] > arr[m+1]){
+				const temp = arr[m+1];
+				arr[m+1] = arr[m];
+				arr[m] = temp;
+			}
+		}
+	}
+
+	return arr;
 }
 
 function selectionSort(arr) {
